31546afefb82e54cc8e341f21309bc1ccfa725b3
Sagi Medina <SagiMedina@Gmail.com>
2016-11-08 20:56:19 +0200

    push github

--- system_dashboard.py
+++ system_dashboard.py
@@ -1,11 +1,8 @@
 # coding:utf-8
 from __future__ import unicode_literals
-
 import json
 import os
 import time
-# import logging
-
 import psutil
 from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
@@ -14,12 +11,7 @@ app = Flask(__name__, static_folder='static')
 
 CORS(app)   # Necessary since API is running locally
 
-# Should match the period (in seconds) in Freeboard
 PERIOD = 1
-
-# Disable Flask console messages: http://stackoverflow.com/a/18379764
-# log = logging.getLogger('werkzeug')
-# log.setLevel(logging.ERROR)
 
 
 @cross_origin()
@@ -32,7 +24,6 @@ def multiple_routes():
 @app.route('/system_data')
 def system_data():
 
-    disk_write_data_start = psutil.disk_io_counters(perdisk=False)
     io_data_start = psutil.net_io_counters()
 
     # Some metrics are only reported in values since uptime,
